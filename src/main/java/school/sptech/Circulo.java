942eec10187187714582aad922a2b2d1b90623bf
ysilvv <yasmim.silva@sptech.school>
2024-11-07 18:39:51 -0300

    Exercicio de Introdução a classe abstrata concluída com sucesso!

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,27 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura {
+    private Double raio;
+
+
+    @Override
+    public Double calcularArea() {
+        Double raioQuadrado = Math.pow(raio, 2);
+        return raioQuadrado * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Circulo{" +
+                "raio=" + raio +
+                '}';
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,46 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras = new ArrayList<>();
+
+
+    public void adicionar(Figura figura) {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas() {
+        Double somaAreas = 0.0;
+
+        for (Figura figura : figuras) {
+            somaAreas += figura.calcularArea();
+
+        }
+        return somaAreas;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20() {
+        List<Figura> figuraAreasMaior = new ArrayList<>();
+
+        for (int i = 0; i < figuras.size(); i++) {
+            Figura figuraDaVez = figuras.get(i);
+            if (figuraDaVez.calcularArea() > 20) {
+                figuraAreasMaior.add(figuraDaVez);
+            }
+        }
+        return figuraAreasMaior;
+    }
+
+    public List<Figura> buscarQuadrados() {
+       List<Figura> quadrados = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+        if (figura instanceof Quadrado) {
+                quadrados.add(figura);
+            }
+        }
+        return quadrados;
+    }
 }
